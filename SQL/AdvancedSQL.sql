0a090be8b43b6c55273cd33bd5a7b37d0bb62f0a
Cansu Naz Sarı <75156558+cansusaarii@users.noreply.github.com>
2024-10-02 15:31:21 +0300

    Add files via upload

---
+++ SQL/AdvancedSQL.sql
@@ -0,0 +1,95 @@
+/*SQL Server does not use AUTOINCREMENT to auto-generate values for primary keys. 
+In SQL Server, we should use the IDENTITY keyword instead of AUTOINCREMENT*/
+
+CREATE TABLE Exercise_logs (Id INT PRIMARY KEY IDENTITY(1,1),
+                           Type TEXT,
+						   Minutes INT,
+						   Calories INT,
+						   Heart_rate INT)
+INSERT INTO Exercise_logs(Type,Minutes,Calories,Heart_rate) VALUES 
+('Biking',30,100,110)
+INSERT INTO Exercise_logs(Type,Minutes,Calories,Heart_rate) VALUES
+('Walking',30,30,105)
+INSERT INTO Exercise_logs(Type,Minutes,Calories,Heart_rate) VALUES
+('Dancing',15,200,120)
+
+SELECT*FROM Exercise_logs
+SELECT*FROM Exercise_logs WHERE Calories>50 ORDER BY Calories
+/* AND */
+SELECT*FROM Exercise_logs WHERE Heart_rate>100 AND Minutes<30
+
+/*Challange 1
+Ever sung karaoke? 
+It's a place where you sing songs with your friends, and it's a lot of fun. 
+We've created a table with songs, and in this challenge, you'll use queries to decide what songs to sing. 
+For the first step, select all the song titles.*/
+
+CREATE TABLE SongsList (
+    SongId INTEGER PRIMARY KEY IDENTITY(1,1),
+    SongTitle TEXT,
+    SongArtist TEXT,
+    Mood TEXT,
+    Duration INTEGER,
+    Released INTEGER)
+
+INSERT INTO SongsList (SongTitle,SongArtist, Mood, Duration, Released)
+    VALUES ('Bohemian Rhapsody', 'Queen', 'epic', 60, 1975)
+INSERT INTO SongsList (SongTitle, SongArtist, Mood, Duration, Released)
+    VALUES ('Let it go', 'Idina Menzel', 'epic', 227, 2013)
+INSERT INTO SongsList (SongTitle, SongArtist, Mood, Duration, Released)
+    VALUES ('I will survive', 'Gloria Gaynor', 'epic', 198, 1978)
+INSERT INTO SongsList (SongTitle, SongArtist, Mood, Duration, Released)
+    VALUES ('Twist and Shout', 'The Beatles', 'happy', 152, 1963)
+INSERT INTO SongsList (SongTitle, SongArtist, Mood, Duration, Released)
+    VALUES ('La Bamba', 'Ritchie Valens', 'happy', 166, 1958)
+INSERT INTO SongsList (SongTitle, SongArtist, Mood, Duration, Released)
+    VALUES ('I will always love you', 'Whitney Houston', 'epic', 273, 1992)
+INSERT INTO SongsList (SongTitle, SongArtist, Mood, Duration, Released)
+    VALUES ('Sweet Caroline', 'Neil Diamond', 'happy', 201, 1969)
+INSERT INTO SongsList (SongTitle, SongArtist, Mood, Duration, Released)
+    VALUES ('Call me maybe', 'Carly Rae Jepsen', 'happy', 193, 2011)
+
+SELECT SongTitle FROM SongsList
+
+/*Maybe your friends only like singing either recent songs or truly epic songs. 
+Add another SELECT that uses OR to show the titles of the songs that have an 'epic' mood 
+or a release date after 1990.*/
+
+ALTER TABLE SongsList ALTER COLUMN Mood VARCHAR(255) /*text to varchar bc cannot be compared using equality*/
+ALTER TABLE SongsList ALTER COLUMN SongTitle VARCHAR(255)
+SELECT SongTitle FROM SongsList WHERE Mood='epic' OR Released>1990
+SELECT SongTitle FROM SongsList WHERE Mood='epic' AND Released>1990 AND Duration<240
+
+INSERT INTO Exercise_logs(Type, Minutes, Calories, Heart_rate) VALUES ('Tree climbing', 30, 70, 90)
+INSERT INTO Exercise_logs(Type, Minutes, Calories, Heart_rate) VALUES ('Tree climbing', 25, 72, 80)
+INSERT INTO Exercise_logs(Type, Minutes, Calories, Heart_rate) VALUES ('Rowing', 30, 70, 90)
+INSERT INTO Exercise_logs(Type, Minutes, Calories, Heart_rate) VALUES ('Hiking', 60, 80, 85)
+
+ALTER TABLE Exercise_logs ALTER COLUMN Type VARCHAR(255)
+
+SELECT * FROM Exercise_logs WHERE Type = 'Biking' OR Type = 'Hiking' OR Type = 'Tree climbing' OR Type = 'Rowing'
+/* IN */
+SELECT * FROM Exercise_logs WHERE Type IN ('Biking','Hiking','Tree climbing','Rowing')
+
+SELECT Type, SUM(Calories) AS Total_Cal FROM Exercise_logs GROUP BY Type
+
+/* HAVING 
+The HAVING command in SQL is used to filter the results of a GROUP BY clause. 
+It works similarly to the WHERE clause, but instead of filtering rows before the grouping, 
+it filters groups after the aggregation has been applied.*/
+
+SELECT Type, SUM(Calories) AS Total_Cal FROM Exercise_logs
+GROUP BY Type
+HAVING SUM(Calories) > 150
+    
+SELECT COUNT(*) FROM Exercise_logs WHERE Heart_rate > 200
+
+/* CASE */
+SELECT Type, Heart_rate,
+    CASE 
+        WHEN Heart_rate > 220-30 THEN 'above max'
+        WHEN Heart_rate > ROUND(0.90 * (220-30),0) THEN 'above target'
+        WHEN Heart_rate > ROUND(0.50 * (220-30),0) THEN 'within target'
+        ELSE 'below target'
+    END as 'HR_zone'
+FROM Exercise_logs
---
+++ SQL/SQLQueryBasics.sql
@@ -0,0 +1,88 @@
+/*Structured Query Language*/
+
+/*Types of SQL Commands
+SQL commands are divided into different categories based on their functionality. The main types are:
+
+DML (Data Manipulation Language):
+DML commands are used to manipulate data stored in database tables. These commands are responsible for retrieving, inserting, modifying, and deleting data.
+
+SELECT: Retrieves data from a table.
+INSERT: Inserts new data into a table.
+UPDATE: Modifies existing data in a table.
+DELETE: Deletes data from a table.*/
+
+/*DDL (Data Definition Language):
+DDL commands define and modify the structure of database objects such as tables, indexes, and schemas. These commands affect the schema of the database.
+
+CREATE: Creates new database objects like tables or indexes.
+ALTER: Modifies existing database objects.
+DROP: Deletes database objects.
+TRUNCATE: Removes all data from a table without deleting the table itself.*/
+
+/** Grocery list: 
+Bananas (4)
+Peanut Butter (1)
+Dark Chocolate Bars (2)
+**/
+
+CREATE TABLE Groceries (GroceryId INT PRIMARY KEY, GroceryName TEXT, Quantity INT)
+
+INSERT INTO Groceries VALUES(1,'Banana',4)
+INSERT INTO Groceries VALUES(2,'Peanut Butter',1)
+INSERT INTO Groceries VALUES(3,'Chocolate Bars',2)
+
+SELECT*FROM Groceries
+
+/*Challange 1
+What are your favorite books? 
+You can make a database table to store them in! 
+In this first step, create a table to store your list of books. 
+It should have columns for id, name, and rating.
+Then add three of books into the table*/
+
+CREATE TABLE My_Library(BookId INT PRIMARY KEY, BookTitle VARCHAR(50), Rating FLOAT)
+INSERT INTO My_Library VALUES(1,'To Kill a Mockingbird',4.8)
+INSERT INTO My_Library VALUES(2,'1984',4.6)
+INSERT INTO My_Library VALUES(3,'The Catcher in the Rye',4.1)
+SELECT * FROM My_Library
+
+SELECT GroceryName FROM Groceries
+SELECT*FROM Groceries ORDER BY Quantity
+
+/*Challange 2
+Filter recent movies
+Select all movies from table first,
+then add a second query after the first, 
+that retrieves only the movies that were released in the year 2000 or later, not before. 
+Sort the results so that the earlier movies are listed first. 
+You should have 2 SELECT statements after this step.*/
+
+CREATE TABLE Movies (MovieId INTEGER PRIMARY KEY, MovieName TEXT, Release_year INTEGER)
+INSERT INTO Movies VALUES (1, 'Avatar', 2009)
+INSERT INTO Movies VALUES (2, 'Titanic', 1997)
+INSERT INTO Movies VALUES (3, 'Star Wars: Episode IV - A New Hope', 1977)
+INSERT INTO Movies VALUES (4, 'Shrek 2', 2004)
+INSERT INTO Movies VALUES (5, 'The Lion King', 1994)
+INSERT INTO Movies VALUES (6, 'Disneys Up', 2009)
+
+SELECT*FROM Movies
+SELECT*FROM Movies WHERE Release_year >= 2000 ORDER BY Release_year
+SELECT MovieName FROM Movies WHERE Release_year >= 2000 ORDER BY Release_year
+
+SELECT SUM(Quantity) FROM Groceries
+SELECT MAX(Quantity) FROM Groceries 
+
+/*Challange 3
+Here's a table containing a TODO list with the number of minutes it will take to complete each item. 
+Insert another item to your todo list with the estimated minutes it will take.
+Select the SUM of minutes it will take to do all of the items on your TODO list. 
+You should only have one SELECT statement.*/
+
+CREATE TABLE Todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER)
+INSERT INTO Todo_list VALUES (1, 'Wash the dishes', 15)
+INSERT INTO Todo_list VALUES (2, 'Vacuuming', 20)
+INSERT INTO Todo_list VALUES (3, 'Learn some stuff on KA', 30)
+INSERT INTO Todo_list VALUES (4,'Drop the garbage',10)
+
+SELECT SUM(minutes) FROM Todo_list 
+SELECT TOP 1 * FROM Todo_list ORDER BY minutes DESC
